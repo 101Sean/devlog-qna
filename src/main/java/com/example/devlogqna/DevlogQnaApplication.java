42e6af7870665b20edcb1fe742ff888326e0f5a8
mikeryu98 <mikeryu98@gmail.com>
2026-04-29 03:30:12 +0900

    v0.1.0 초기 커밋

---
+++ src/main/java/com/example/devlogqna/DevlogQnaApplication.java
@@ -0,0 +1,13 @@
+package com.example.devlogqna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevlogQnaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevlogQnaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devlogqna/DevlogQnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devlogqna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevlogQnaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
